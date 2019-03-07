18e7e722f059fe87dfb079b589c1862c733d141b
PinPoint <ms1b.139@gmail.com>
2019-03-07 16:57:51 +0100

    added separate folder for project 1

---
+++ projekt_1/Zad2b.m
@@ -0,0 +1,21 @@
+% clear Y;
+% 
+% Y = 2*ones(200,1);
+% 
+% U(1:10) = 1.1;
+% %Ukon = 0.95;
+% %kwyk = 1:1:200;
+% Ustat = 1.1:0.001:1.299;
+%     
+% for k = 12:1:200
+%    Y(k) = symulacja_obiektu3Y(Ustat(k-10),Ustat(k-11),Y(k-1),Y(k-2));
+% end
+% 
+%     %axes ([1 21 1.9 2.1])
+%     
+% plot(Ustat, Y);
+%     
+% %Ukon = Ukon + 0.03;
+
+
+
