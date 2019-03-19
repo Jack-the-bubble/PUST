568d514642bcb17759c549f86e12e7e986f61a8a
Micha <mlk04@wp.pl>
2019-03-19 23:40:36 +0100

    generated data to txt

--- projekt_1/zad1_ekstra.m
+++ projekt_1/zad1_ekstra.m
@@ -6,11 +6,11 @@ clear U;
 
 Y(1:151) = 4;
 U(1:151) = 1.1;
-
+iterNum=151;
 for k = 12:1:151
     Y(k) = symulacja_obiektu3Y(U(k-10),U(k-11),Y(k-1),Y(k-2));
 end
-U1
+Y1=Y;
 %punkt pracy z zadania 1 poprawny, poniewa� warto�ci Y po uruchomieniu
 %skryptu pozosta�y 2
 figure(1)
@@ -22,11 +22,10 @@ stairs(U)
 hold off
 
 Y(1:151) = 3;
-
 for k = 12:1:151
     Y(k) = symulacja_obiektu3Y(U(k-10),U(k-11),Y(k-1),Y(k-2));
 end
-
+Y2=Y;
 hold on
 stairs(Y)
 hold off
@@ -35,23 +34,38 @@ stairs(U)
 hold off
 
 Y(1:151) = 2;
-
 for k = 12:1:151
     Y(k) = symulacja_obiektu3Y(U(k-10),U(k-11),Y(k-1),Y(k-2));
 end
-
+Y3=Y;
 hold on
 stairs(Y)
 hold off
 hold on
 stairs(U)
 hold off
+
 nazwa1 = 'od_4.txt';
 nazwa2 = 'od_3.txt';
 nazwa3 = 'od_2.txt';
 nazwa4 = 'U.txt';
  
 file = fopen(nazwa1, 'w');
-A = [(1:iterNum);U'];
+A = [(1:iterNum);Y1];
+fprintf(file, '%4.3f %.3f \n',A);
+fclose(file);
+
+file = fopen(nazwa2, 'w');
+A = [(1:iterNum);Y2];
+fprintf(file, '%4.3f %.3f \n',A);
+fclose(file);
+
+file = fopen(nazwa3, 'w');
+A = [(1:iterNum);Y3];
+fprintf(file, '%4.3f %.3f \n',A);
+fclose(file);
+
+file = fopen(nazwa4, 'w');
+A = [(1:iterNum);U];
 fprintf(file, '%4.3f %.3f \n',A);
 fclose(file);
